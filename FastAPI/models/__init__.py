961418bc1466b487f0b12bfa29e7240008221e74
Dbaird2 <161085580+Dbaird2@users.noreply.github.com>
2026-06-10 13:02:46 -0600

    updating requirements

---
+++ FastAPI/models/__init__.py
@@ -0,0 +1,8 @@
+from models.OCR_Model import OCRResult, AddPersonModel, EditPersonModel, SearchPersonModel
+
+__all__ = [
+    "OCRResult",
+    "AddPersonModel",
+    "EditPersonModel",
+    "SearchPersonModel",
+]
